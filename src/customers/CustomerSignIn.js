91891aec9e297f195123166253253568e29ee4db
Aravind R Pillai <aravindrpillai1992@gmail.com>
2023-05-18 15:41:53 +0100

    Unused imports

--- src/customers/CustomerSignIn.js
+++ src/customers/CustomerSignIn.js
@@ -1,10 +1,7 @@
 import React from "react";
-import Avatar from "@material-ui/core/Avatar";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Paper from "@material-ui/core/Paper";
-import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
-import PhotoCameraIcon from '@mui/icons-material/PhotoCamera';
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Copyright from "../Components/Copyright";
